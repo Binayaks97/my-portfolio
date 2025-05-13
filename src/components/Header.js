02b142cd19f7de6b183c017e5cd5f6151b35b423
Binayak Sahoo <binayaksahoo1997@gmail.com>
2025-05-13 16:36:02 +0530

    Updated favicon and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <nav className="navbar navbar-expand-lg bg-custom-blue navbar-custom-text fixed-top shadow slide-header">
-    <div className="container">
-      <Link className="navbar-brand fw-bold" to="/">
-        Binayak Sahoo
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/skills">
-              Skills
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/projects">
-              Projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/experience">
-              Experience
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
+const Header = () => {
+  const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
+
+  const handleNavLinkClick = () => {
+    setIsNavbarCollapsed(true); // Collapse menu after link click
+  };
+
+  const toggleNavbar = () => {
+    setIsNavbarCollapsed(!isNavbarCollapsed); // Toggle menu open/close
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-custom-blue navbar-custom-text fixed-top shadow slide-header">
+      <div className="container">
+        <Link className="navbar-brand fw-bold" to="/" onClick={handleNavLinkClick}>
+        <img
+          src="/MyLogo.png"
+          alt="Binayak Sahoo Logo"
+          style={{ height: '80px', width: '100px', marginRight: '10px' }}
+        />
+          Binayak Sahoo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+          aria-expanded={!isNavbarCollapsed}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${!isNavbarCollapsed ? 'show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about" onClick={handleNavLinkClick}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/skills" onClick={handleNavLinkClick}>
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects" onClick={handleNavLinkClick}>
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/experience" onClick={handleNavLinkClick}>
+                Experience
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact" onClick={handleNavLinkClick}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Header;
